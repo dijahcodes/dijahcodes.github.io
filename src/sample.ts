c306a7b8ba161862a577dd3658772d42247c1c4f
Dijah <5828360+dijahcodes@users.noreply.github.com>
2026-01-06 20:28:24 -0500

    copy changes, editing layout
    -removed testimonials for the layout
    -removed two of the demo blogs
    -renamed example projects for now
    -edited sample.ts (AUTHOR INFO)

--- src/sample.ts
+++ src/sample.ts
@@ -1,17 +1,17 @@
 const author = {
-    name: `Angela`,
-    nickname: `angie`,
-    tiktok: `https://www.tiktok.com/@codetonero`,
-    ig: `https://www.instagram.com/codetonero/`,
-    x: `https://x.com/codetonero`,
-    email: 'mailto:anthonylannn@gmail.com'
+    name: `Dijah`,
+    nickname: `dijah`,
+    tiktok: `https://www.tiktok.com/`,
+    ig: `https://www.instagram.com/dijahcodes/`,
+    x: `https://x.com/dijahcodes`,
+    //email: 'mailto:anthonylannn@gmail.com'
 
 }
 
 
 const buy = {
     title: `See on Github`,
-    link: `https://github.com/anthonylan/angie`
+    link: `https://github.com/anthonylan/dijahcodes`
 }
 
 
